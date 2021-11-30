d38b7451164150bab23543f2b32e2b05246d3cb2
levi <glendenning.levi@gmail.com>
2021-11-30 16:21:58 -0700

    finished video 14

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,19 +2,34 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from "../sample-fishes";
 
 class App extends React.Component{
-render() {
-  return(
-    <div className="catch-of-the-day">
-      <div className="menu">
-        <Header tagline="Fresh Seafood Market" />
+  state = {
+    fishes: {},
+    order: {}
+  }
+  addFish = fish => {
+    // 1/ take a copy of the exsiting state
+    const fishes ={ ...this.state.fishes};
+    // 2. add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // 3.set the new fishes object to state
+    this.setState({ fishes });
+  }
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  }
+  render() {
+    return(
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+        </div>
+        <Order />
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFish} />
       </div>
-      <Order />
-      <Inventory />
-    </div>
-  );
-}
-}
-
+    );
+  }
+  };
 export default App;
---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div>
+    <h2> Not Found!!!!!</h2>
+    <p> Sorry your page was not found, try again !!</p>
+  </div>
+);
+
+export default NotFound;
