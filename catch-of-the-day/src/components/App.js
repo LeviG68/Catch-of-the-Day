b54ef0079adc1acd6b64a8360cfd1264de684746
levi <glendenning.levi@gmail.com>
2021-12-02 14:36:01 -0700

    finished video 15

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends React.Component{
   state = {
@@ -11,7 +12,7 @@ class App extends React.Component{
   }
   addFish = fish => {
     // 1/ take a copy of the exsiting state
-    const fishes ={ ...this.state.fishes};
+    const fishes = { ...this.state.fishes};
     // 2. add our new fish to that fishes variable
     fishes[`fish${Date.now()}`] = fish;
     // 3.set the new fishes object to state
@@ -25,11 +26,19 @@ class App extends React.Component{
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish 
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}/>
+            ))}
+          </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFish} />
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>
     );
   }
-  };
+};
 export default App;
--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -5,11 +5,14 @@ class Inventory extends React.Component {
   render() {
     return (
       <div className="inventory">
-        <h2> Inventory !! </h2>
+        <h2>Inventory</h2>
         <AddFishForm addFish={this.props.addFish} />
-        <button onClick={this.props.loadSampleFishes}>Load Sample Button</button>
+        <button onClick={this.props.loadSampleFishes}>
+          Load Sample Fishes
+        </button>
       </div>
     );
   }
 }
- export default Inventory;
+
+export default Inventory;
