c5b218527e0a272172a15616df43d9cbc8e44798
levi <glendenning.levi@gmail.com>
2022-01-11 11:19:20 -0700

    finished adding all proptypes to application

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,9 +1,15 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { formatPrice } from "../helper";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 
 class Order extends React.Component {
+  static propTypes = {
+    fishes: PropTypes.object,
+    order: PropTypes.object, 
+    removeFromOrder: PropTypes.number
+  }
   renderOrder = (key) => {
     const fish = this.props.fishes[key];
     const count = this.props.order[key];
